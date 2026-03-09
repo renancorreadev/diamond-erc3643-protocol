693130848bf9b365f20746c31f5637b78f0a317a
Renan Correa <renan.correa@hubweb3.com>
2026-03-09 11:22:31 -0300

    test(unit): update facet count assertions to reflect 7-facet Diamond

--- packages/contracts/test/unit/Diamond.t.sol
+++ packages/contracts/test/unit/Diamond.t.sol
@@ -40,7 +40,7 @@ contract DiamondTest is DiamondHelper {
 
     function test_FacetAddressesReturnsThreeFacets() public view {
         address[] memory addrs = IDiamondLoupe(address(d.diamond)).facetAddresses();
-        assertEq(addrs.length, 3);
+        assertEq(addrs.length, 7); // cut + loupe + ownership + accessControl + pause + emergency + freeze
     }
 
     /*//////////////////////////////////////////////////////////////
--- packages/contracts/test/unit/DiamondLoupe.t.sol
+++ packages/contracts/test/unit/DiamondLoupe.t.sol
@@ -23,7 +23,7 @@ contract DiamondLoupeTest is DiamondHelper {
 
     function test_FacetAddressesLength() public view {
         address[] memory addrs = loupe.facetAddresses();
-        assertEq(addrs.length, 3); // cut + loupe + ownership
+        assertEq(addrs.length, 7); // cut + loupe + ownership + accessControl + pause + emergency + freeze
     }
 
     function test_FacetAddressesContainsCutFacet() public view {
@@ -96,7 +96,7 @@ contract DiamondLoupeTest is DiamondHelper {
 
     function test_FacetsReturnsCorrectLength() public view {
         IDiamondLoupe.Facet[] memory fs = loupe.facets();
-        assertEq(fs.length, 3);
+        assertEq(fs.length, 7);
     }
 
     function test_FacetsSelectorsNonEmpty() public view {
